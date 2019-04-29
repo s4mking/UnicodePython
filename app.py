276aafa917cb7747a6f95684ae7a378776db03ae
s4mking <samuel.simonney@supinternet.fr>
2019-04-29 10:31:04 +0200

    Ajout form search en GET + redirection

--- app.py
+++ app.py
@@ -7,23 +7,22 @@ app = Flask(__name__)
 @app.route('/')
 def template_test():
     return render_template('home.html')
-    
-@app.route('/search/')
-def hello_word():
-    return render_template('search.html')
+
+@app.route('/search/',methods = ['POST', 'GET'])
+def search():
+     if request.method == 'GET' and bool(request.args):
+        result = request.args["search"]
+        result_render = utils.findUnicode(result)
+        return render_template("result.html",len = len(result_render),result_render=result_render)
+     else:
+        return render_template('search.html')
 
 @app.route('/list/')
 def list_all():
     result_render = utils.getAllInformations()
     print(result_render)
     return render_template("list.html",len = len(result_render),tabs=result_render)
-
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-       result = request.form["search"]
-       result_render = utils.findUnicode(result)
-       return render_template("result.html",len = len(result_render),result_render=result_render)
+  
 @app.route('/unicode/<codepoint>')
 def matchsasa_uni(codepoint):
     print(codepoint)
